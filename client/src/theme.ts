edc57a4eb093732f7a61b7f99bd83c99f1449315
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2024-03-28 17:49:59 +0200

    add styles, colors, files

---
+++ client/src/theme.ts
@@ -0,0 +1,26 @@
+export const theme = {
+  colors: {
+    white: "#FFFFFF",
+    grey: "#5E6381",
+    whiteGray: "#E1E3EA",
+    prioriryGrey: "#F3F4F6",
+    red: "#FF4040",
+    black: "#303030",
+  },
+  radius: {
+    sm: "10px",
+    md: "20px",
+    lg: "100px",
+  },
+  fonts: {
+    smSize: "18px",
+    mdSize: "20px",
+    bigSize: "24px",
+
+    md: "500",
+    bold: "600",
+  },
+  transition: {
+    transitionDurAndFunc: "350ms cubic-bezier(0.4, 0, 0.2, 1)",
+  },
+};
